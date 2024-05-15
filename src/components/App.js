4104aff990fd227fcc6c80e341b19a1bab3b9cf8
Simone Veitch <simone.veitch@gmail.com>
2024-05-15 10:58:10 +1000

    Completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
-function App() {
+function App( {handleAddItem }) {
   const [page, setPage] = useState("List");
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddItem={handleAddItem} /> : <QuestionList />}
     </main>
   );
 }
